62b026ed32b0efa5a4afd003ebdde647126d36e0
Elieuton da silva lemos junior <160804009+lemosjr@users.noreply.github.com>
2025-06-24 21:11:10 -0300

    Add menu selection script for telecom options
    Introduces a Python script that prompts the user to select a telecom provider and prints a welcome message based on the selection. Handles invalid input with an error message.

---
+++ Aula-7/exercicios.py
@@ -0,0 +1,17 @@
+menu = input('''
+                    1 - Tim
+                    2 - vivo
+                    3 - claro
+                    4 - brisa 
+          
+          escolha uma das opções:''')
+if menu == 1:
+    print("Bem vindo a Tim")
+elif menu == 2:
+    print("Bem vindo a Vivo")
+elif menu == 3:
+    print("Bem vindo a Claro")
+elif menu == 4:
+    print("Bem vindo a Brisanet")
+else:
+    print("número inválido")
