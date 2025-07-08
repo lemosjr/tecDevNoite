9a43b0a0c9cf3828b3238767fd255ce2ab913400
Elieuton da silva lemos junior <160804009+lemosjr@users.noreply.github.com>
2025-07-07 21:05:24 -0300

    Add blank lines at end of ex05.py
    Appended blank lines to the end of the ex05.py file to comply with formatting standards.

--- Aula-13/ex05.py
+++ Aula-13/ex05.py
@@ -3,3 +3,6 @@
 # retorne True se a string for um palíndromo e False caso contrário.
 # (Uma string é um palíndromo se lê da mesma forma de trás para frente.)
 # Dica pesquise sobre conceito de slicing
+
+
+
