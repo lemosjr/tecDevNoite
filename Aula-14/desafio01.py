0d64d68cf9368429bd003bb0c2533aa62ec3758e
lemosjr <160804009+lemosjr@users.noreply.github.com>
2025-07-10 18:54:42 -0300

    Refactor even/odd list processing logic
    Updated iteration to use index-based loops in both desafio01.py and desafio02.py. In desafio01.py, even numbers are now replaced with 0 instead of inserting 0. In desafio02.py, indices of even and odd numbers are appended to separate lists instead of assigning lists to elements.

--- Aula-14/desafio01.py
+++ Aula-14/desafio01.py
@@ -3,7 +3,7 @@
 
 lista = [1,2,3,4,5,6,7,8,9,10]
 
-for i in lista:
+for i in range(len(lista)):
     if lista[i] % 2 == 0:
-        lista.insert(i, 0)
+        lista[i] = 0
     print(lista)
--- Aula-14/desafio02.py
+++ Aula-14/desafio02.py
@@ -5,11 +5,11 @@
 lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 par = []
 impar = []
-for i in lista:
+for i in range(len(lista)):
     if lista[i] % 2 == 0: 
-        lista[i] = par
+        par.append(i)
     else:
-        lista[i] = impar
+        impar.append(i)
 
 print(par)
 print(impar)
