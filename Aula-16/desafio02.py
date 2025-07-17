fb501cb530c03702d5aee01ddce184c51c92d36d
lemosjr <160804009+lemosjr@users.noreply.github.com>
2025-07-16 21:13:43 -0300

    Add interactive menu for stock management
    Introduces a menu-driven interface to view, add, remove, and subtract items from the store's stock. Replaces the previous static product query with modular functions for each operation and calls the menu on script start.

--- Aula-16/desafio02.py
+++ Aula-16/desafio02.py
@@ -8,10 +8,52 @@ estoque_loja = {
     "laranja": 20,
     "uva": 15
 }
+def menu():
+    while True:
+        menu = input(f'''
+                        1 - ver estoque
+                        2 - adicionar ao estoque
+                        3 - remover do estoque
+                        4 - subtrair do estoque
+                        5 - sair  
+                        Digite a entrada:''')
+        if menu == '1':
+            verEstoque()
+        elif menu == '2':
+            adcEstoque()
+        elif menu == '3':
+            rmvEstoque()
+        elif menu == '4':
+            dimEstoque()
+        elif menu == '5':
+            return
 
-produto = input("Digite o nome do produto: ")
+def verEstoque():
+    print(estoque_loja)
 
-if produto in estoque_loja:
-    print("Quantidade em estoque:", estoque_loja[produto])
-else:
-    print("Produto não disponível.")
+def adcEstoque():
+    adcProduto = input("Escreva o produto que deseja adicionar:")
+    qtdProduto = int(input("Digite a quantidade a ser adicionada ao estoque:"))    
+    estoque_loja[adcProduto] = qtdProduto
+    if adcProduto in estoque_loja:
+        qtdProduto += qtdProduto
+    
+    print(estoque_loja)    
+
+def rmvEstoque():   
+    remover = input("Digite o item a ser removido:") 
+    del estoque_loja[remover]
+    
+def dimEstoque():
+    ant = 2
+    print(ant)
+
+
+
+# produto = input("Digite o nome do produto: ")
+
+# if produto in estoque_loja:
+#     print("Quantidade em estoque:", estoque_loja[produto])
+# else:
+#     print("Produto não disponível.")
+menu()
